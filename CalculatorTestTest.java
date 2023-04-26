e332156503ce98e79c6c654a5dc3133b1b49378d
Sylwia Kwiatkowska <kwiatkowska.sylwia97@wp.pl>
2023-04-26 23:32:24 +0200

    2 commit

---
+++ CalculatorTestTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTestTest {
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubstract() {
+    }
+
+    @Test
+    public void testMultiply() {
+    }
+
+    @Test
+    public void testDivide() {
+    }
+}

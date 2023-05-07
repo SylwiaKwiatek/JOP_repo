78876f0b98f4cf6b7900db6b7383def2f530c7bd
Sylwia Kwiatkowska <kwiatkowska.sylwia97@wp.pl>
2023-05-07 21:16:53 +0200

    Update Calculator: add enum, split the method into several smaller Update CalculatorTest: add content

--- Calculator.java
+++ Calculator.java
@@ -1,55 +1,43 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double secondNumber, result;
-        String operation;
-        String answer = "y";
 
         System.out.println("Enter first number: ");
-        while (!scanner.hasNextDouble()) {
-            System.out.println("Invalid input! Please enter a number.");
-            scanner.next();
-        }
-        result = scanner.nextDouble();
+        double result = getValidDoubleInput(scanner);
 
+        String answer = "y";
         while (answer.equals("y")) {
             System.out.println("Enter second number: ");
-            while (!scanner.hasNextDouble()) {
-                System.out.println("Invalid input! Please enter a number.");
-                scanner.next();
-            }
-            secondNumber = scanner.nextDouble();
+            double secondNumber = getValidDoubleInput(scanner);
 
             System.out.println("Choose an operation: addition (+), subtraction (-), multiplication (*) or division (/): ");
-            operation = scanner.next();
+            char operation = getValidOperation(scanner);
 
             switch (operation) {
-                case "+":
-                    System.out.println(result + " " + "+" + " " + secondNumber + " " + "=" + " " + (result += secondNumber));
+                case '+':
+                    result = add(result, secondNumber);
+                    System.out.println(result);
                     break;
-                case "-":
-                    System.out.println(result + " " + "-" + " " + secondNumber + " " + "=" + " " + (result -= secondNumber));
+                case '-':
+                    result = subtract(result, secondNumber);
+                    System.out.println(result);
                     break;
-                case "*":
-                    System.out.println(result + " " + "*" + " " + secondNumber + " " + "=" + " " + (result *= secondNumber));
+                case '*':
+                    result = multiply(result, secondNumber);
+                    System.out.println(result);
                     break;
-                case "/":
+                case '/':
                     while (secondNumber == 0) {
                         System.out.println("Remember that division by zero is not allowed!");
                         System.out.println("Enter the second number: ");
-                        while (!scanner.hasNextDouble()) {
-                            System.out.println("Invalid input! Please enter a number.");
-                            scanner.next();
-                        }
-                        secondNumber = scanner.nextDouble();
+                        secondNumber = getValidDoubleInput(scanner);
                     }
-                    System.out.println(result + " " + "/" + " " + secondNumber + " " + "=" + " " + (result /= secondNumber));
+                    result = divide(result, secondNumber);
+                    System.out.println(result);
                     break;
-                default:
-                    System.out.println("Invalid operation selected.");
             }
 
             System.out.println("Do you want to continue calculations on the obtained number? (y/n)");
@@ -59,22 +47,41 @@ public class Calculator {
                 answer = scanner.next();
             }
         }
+
         System.out.println("Final result: " + result);
     }
 
-    public double add(double v1, double v2) {
+    public static double getValidDoubleInput(Scanner scanner) {
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Invalid input! Please enter a number.");
+            scanner.next();
+        }
+        return scanner.nextDouble();
+    }
+
+    public static char getValidOperation(Scanner scanner) {
+        char operation = scanner.next().charAt(0);
+        while (operation != '+' && operation != '-' && operation != '*' && operation != '/') {
+            System.out.println("Invalid operation selected. Please choose a valid operation: addition (+), subtraction (-), multiplication (*) or division (/)");
+            operation = scanner.next().charAt(0);
+        }
+        return operation;
+    }
+
+    public static double add(double v1, double v2) {
         return v1 + v2;
     }
 
-    public double substract(double v1, double v2) {
+    public static double subtract(double v1, double v2) {
         return v1 - v2;
     }
 
-    public double multiply(double v1, double v2) {
+    public static double multiply(double v1, double v2) {
         return v1 * v2;
     }
 
-    public double divide(double v1, double v2) {
+    public static double divide(double v1, double v2) {
         return v1 / v2;
     }
+
 }

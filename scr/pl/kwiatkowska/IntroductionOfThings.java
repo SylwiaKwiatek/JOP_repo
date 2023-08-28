4008b826ebccfec74dd4c20ca26a261a475461f8
Sylwia Kwiatkowska <kwiatkowska.sylwia97@wp.pl>
2023-08-28 15:23:57 +0200

    Add Human class

--- scr/pl/kwiatkowska/IntroductionOfThings.java
+++ scr/pl/kwiatkowska/IntroductionOfThings.java
@@ -32,6 +32,16 @@ class Mammal extends Animal {
     }
 }
 
+class Human extends Mammal {
+    public Human(String name, int age) {
+        super(name, age);
+    }
+
+    public void speech() {
+        System.out.println(name + " can communicate by speech.");
+    }
+}
+
 class Bird extends Animal {
     public Bird(String name, int age) {
         super(name, age);
@@ -63,12 +73,18 @@ class Thing extends NonLivingThing {
 public class IntroductionOfThings {
     public static void main(String[] args) {
         Mammal cat = new Mammal("Cat", 3);
+        Human Mary = new Human ("Mary", 28);
         Bird sparrow = new Bird("Sparrow", 1);
         Thing book = new Thing("Book");
 
         cat.makeSound();
         cat.introduce();
         cat.feedMilk();
+
+        Mary.makeSound();
+        Mary.introduce();
+        Mary.feedMilk();
+        Mary.speech();
 
         sparrow.makeSound();
         sparrow.introduce();
